123c47f08a113789c63e03c124a89a24847dfbc5
ZhengTian <tangzt96@gmail.com>
2021-12-15 15:28:34 +0800

    filled up different pages

--- SketchConverter.py
+++ SketchConverter.py
@@ -52,14 +52,16 @@ rad = st.sidebar.radio("Navigation",["QR Helper", "About SmartGym","Campaigns"])
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
-img = Image.open("screenshot_guide.png")
-image = st.image(img)
 
-uploaded_file = st.sidebar.file_uploader(" ", type=['png', 'jpg', 'jpeg'])
+
+#uploaded_file = st.sidebar.file_uploader(" ", type=['png', 'jpg', 'jpeg'])
 
 # if uploaded_file is not None:  
 #     image.image(uploaded_file)
 if rad == "QR Helper":
+    img = Image.open("screenshot_guide.png")
+    image = st.image(img)
+    uploaded_file = st.sidebar.file_uploader(" ", type=['png', 'jpg', 'jpeg'])
     if st.sidebar.button("Enhance QR"):
         
         if uploaded_file is None:
@@ -90,41 +92,22 @@ if rad == "QR Helper":
             st.error("Please upload a image first")
 
 if rad == "About SmartGym":
-    st.markdown(
-                """## Contributions
-                    This an open source project and you are very welcome to **contribute** your awesome
-                    comments, questions, resources and apps as
-                    [issues](https://github.com/MarcSkovMadsen/awesome-streamlit/issues) or
-                    [pull requests](https://github.com/MarcSkovMadsen/awesome-streamlit/pulls)
-                    to the [source code](https://github.com/MarcSkovMadsen/awesome-streamlit).
-                    For more details see the [Contribute](https://github.com/marcskovmadsen/awesome-streamlit#contribute) section of the README file.
-                    ## The Developer
-                    This project is developed by Marc Skov Madsen. You can learn more about me at
-                    [datamodelsanalytics.com](https://datamodelsanalytics.com).
-                    Feel free to reach out if you wan't to join the project as a developer. You can find my contact details at [datamodelsanalytics.com](https://datamodelsanalytics.com).
-                    [<img src="https://github.com/MarcSkovMadsen/awesome-streamlit/blob/master/assets/images/datamodelsanalytics.png?raw=true" style="max-width: 700px">](https://datamodelsanalytics.com)
-                    """,
-                                unsafe_allow_html=True,
-            )
+    st.title("SmartGym – Every Citizen’s #1 Fitness Lifestyle Companion")
+    st.text("SmartGym is a holistic fitness data platform that provides users with fitness insights measured through a series of connected sensors built into gym equipment. These sensors are currently installed on three types of gym equipment: 1) weight stack machines, 2) treadmills and 3) weighing machines. SmartGym equipment is currently deployed in three community gyms across Singapore – Our Tampines Hub, Jurong East and Heartbeat @ Bedok.")
+    st.subheader("Weight Stack Machines")
+    st.text("The SmartGym sensors are installed within the covers of the weight stack exteriors, are visible to users and help detect the weights lifted to accurately compute the number of repetitions done.")
     weight_stack_img = Image.open("smartgym-shoulder-press.png")
     st.image(weight_stack_img)
 
+    st.subheader("How Will SmartGym Benefit Users?")
+    st.text("Where SmartGym separates itself from regular gym equipment is its ability to accurately monitor and store users’ data. In a typical gym, fitness equipment can only display workout information while it is being used and the information does not get stored thereafter. In contrast, SmartGym stores workout information that can be viewed at any time at any of the three SmartGym kiosks.")
+    console_img = Image.open("smartgym-console.png")
+    st.image(console_img)
+
+
 if rad == "Campaigns":
-    st.markdown(
-                """## Contributions
-                    This an open source project and you are very welcome to **contribute** your awesome
-                    comments, questions, resources and apps as
-                    [issues](https://github.com/MarcSkovMadsen/awesome-streamlit/issues) or
-                    [pull requests](https://github.com/MarcSkovMadsen/awesome-streamlit/pulls)
-                    to the [source code](https://github.com/MarcSkovMadsen/awesome-streamlit).
-                    For more details see the [Contribute](https://github.com/marcskovmadsen/awesome-streamlit#contribute) section of the README file.
-                    ## The Developer
-                    This project is developed by Marc Skov Madsen. You can learn more about me at
-                    [datamodelsanalytics.com](https://datamodelsanalytics.com).
-                    Feel free to reach out if you wan't to join the project as a developer. You can find my contact details at [datamodelsanalytics.com](https://datamodelsanalytics.com).
-                    [<img src="https://github.com/MarcSkovMadsen/awesome-streamlit/blob/master/assets/images/datamodelsanalytics.png?raw=true" style="max-width: 700px">](https://datamodelsanalytics.com)
-                    """,
-                                unsafe_allow_html=True,
-            )
+    st.title("Current Campaigns in SmartGym!")
+    st.text("We have awesome monthly leaderboard challenge and campaigns with attractive prizes")
+    st.subheader("1 for 1 promotion")
     promo_img = Image.open("promo.jpeg")
     st.image(promo_img)
